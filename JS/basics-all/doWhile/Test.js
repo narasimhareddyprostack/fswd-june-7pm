5ee555a2f99e0a361400f7a2a2eaadb5647db4c9
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-06-24 19:44:44 +0530

    variables

---
+++ JS/basics-all/doWhile/Test.js
@@ -0,0 +1,15 @@
+for (let i = 10; i <= 5; i--) {
+  console.log("for - Loop: Value:", i);
+}
+
+/*
+let i =10;
+i<=5
+i = i-1
+
+*/
+var i = 10;
+do {
+  console.log("value: ", i);
+  i = i - 1;
+} while (i <= 5);
---
+++ JS/basics-all/doWhile/one.js
@@ -0,0 +1,18 @@
+// WAP - first 10 even number  number
+//0,2,4,6,8,10,12,14,16,18
+/* for (let i = 0; i <= 18; i = i + 2) {
+  console.log(i);
+}
+ */
+
+/* let i = 0;
+while (i < 20) {
+  console.log(i);
+  i = i + 2;
+}
+ */
+let i = 0;
+do {
+  console.log(i);
+  i = i + 2;
+} while (i <= 18);
